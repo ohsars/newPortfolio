4af70dadff20efb5130e0492206b0240475c1bd7
Marvelous Osakpolor <efebeosas@gmail.com>
2024-04-09 22:30:41 +0100

    changed the gap between isons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ export const Contact = () => {
           <h3 className='text-xl font-bold'>Hit me Up! 👇🏾</h3>
         </div>
 
-        <div className='flex mt-8 gap-16 flex-wrap'>
+        <div className='flex mt-8 gap-8 md:gap-16 flex-wrap'>
           <div className='flex justify-center gap-5 items-center leading-loose'>
             <i className='fa fa-location md:text-2xl'/>
             <div>
@@ -30,7 +30,7 @@ export const Contact = () => {
             <i className='fa fa-phone md:text-2xl'/>
             <div>
               <h2 className='text-sm font-medium'>Phone</h2>
-              <p className='text-xs leading-normal'><a href='call:+2348161548772'>(+234) 0 816 154 8772</a></p>
+              <p className='text-xs leading-normal'><a href='phone:+2348161548772'>(+234) 0 816 154 8772</a></p>
             </div>
           </div>
         </div>
